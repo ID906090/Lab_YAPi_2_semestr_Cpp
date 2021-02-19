25ed1ee99921cb180d19202e9cc70e35948ae852
Stanislav <79320242+M07i4ka-Stanislav@users.noreply.github.com>
2021-02-19 20:29:47 +0600

    Add files via upload

---
+++ Stanislav_labNum_1.cpp
@@ -0,0 +1,50 @@
+﻿/*
+    Author: Naumov Stanislav
+
+    Group: SBS-001-O-01
+
+    Task: 10
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double height();
+
+int main()
+{
+    cout << "This program calculates the acceleration of gravity of a body relative to the earth's surface.";
+
+    //Объявляем значения - константы(Гравит. постоянная, Масса земли, радиус земли соответственно)
+    const double G = (6.672 * pow(10, -11));
+    const double M = (5.96 * pow(10, 24));
+    const double R = (6.37 * pow(10, 6));
+    
+    double H = height();
+    
+    double g = (G * ( M / (pow((R + H), 2))));
+   
+    cout << "\nFree fall acceleration of a given body = " << g ;
+   
+    return 0;
+}
+
+double height()
+{
+    while (true) //Цикл работает до тех пор, пока пользователь не введет корректное значение 
+    {
+        cout << "\nEnter the distance from the ground (in meters): "; //Запрашиваем данные о высоте над поверхностью земли
+        double H;
+        cin >> H;
+
+        if (cin.fail() || H <= 0) //Если предыдущее извлечение оказалось неудачным и программа ушла в "режим отказа" или H <= 0, то 
+        {
+            cin.clear(); // возвращаем программу в "нормальный режим"
+            cin.ignore(32800, '\n'); // и игнорируем 32 800 значений из входного буфера вплоть до появления символа '\n' (который также удаляем)
+        }
+        else //Если с вводом все впорядке - возвращаем H
+            return H;
+    }
+}
---
+++ Stanislav_labNum_2.cpp
@@ -0,0 +1,55 @@
+﻿/*
+    Author: Naumov Stanislav
+
+    Group: SBS-001-O-01
+
+    Task: 10
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int upperBound();
+
+int main()
+{
+    cout << "|| This program calculates the product. ||";
+
+    const double Pi = 3.14; //Объявляем переменные постоянную переменную Пи
+    double res = 1;
+
+    int N = upperBound();
+
+    for (int n = 1; n <= N; n++) { //Вычисляем произведенние с указанной пользователем вверхней границы(N)
+        res *= (cos(Pi / (pow(2, (n + 1)))));
+    }
+    cout << "|| Result product: " << res << endl;
+    
+
+    return 0;
+}
+
+int upperBound()
+{
+    while (true) //Цикл работает до тех пор, пока пользователь не введет корректное значение 
+    {
+        cout << "\n|| Enter the upper limit of the produkt: "; //Запрашиваем данные о верхней границы произведения
+        int N;
+        cin >> N;
+
+        if (cin.fail() || N < 0) //Если предыдущее извлечение оказалось неудачным и программа ушла в "режим отказа" или N <= 0, то 
+        {
+            cin.clear(); // возвращаем программу в "нормальный режим"
+            cin.ignore(32800, '\n'); // и игнорируем 32 800 значений из входного буфера вплоть до появления символа '\n' (который также удаляем)
+        }
+        else //Если с вводом все впорядке - возвращаем N
+        {
+            cin.ignore(32800, '\n'); //Удаляем лишнии значения(если пользователь ввел слишком большое числовое значение)
+
+            return N;
+        }
+    }
+}
+
